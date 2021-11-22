c962ec68036225b88fd5a1c1bb0da98961dace91
Keith Evans <11130585+derhexenmeister@users.noreply.github.com>
2021-11-22 09:49:48 -0800

    Move test lists into subdirectories. Update dot tests since it's now treated as an atom

--- examples/runall.sh
+++ examples/runall.sh
@@ -5,7 +5,7 @@ echo "Running ff"
 awk -f process.awk \
 	-v TEMPLISPFMT="preprocessed/ff/program%d.lisp" \
        	-v TEMPLISPMETAFMT="preprocessed/ff/program_meta%d.lisp" \
-	< ff.txt
+	< metacircular/ff.txt
 
 echo "----------------------------------------------------------------------"
 echo "Running ff embedded in metacircular evaluator"
@@ -13,14 +13,14 @@ awk -f process.awk \
 	-v RUNMETACIRCULAR=1 \
 	-v TEMPLISPFMT="preprocessed/ff/program%d.lisp" \
        	-v TEMPLISPMETAFMT="preprocessed/ff/program_meta%d.lisp" \
-	< ff.txt
+	< metacircular/ff.txt
 
 echo "======================================================================"
 echo "Running ff preembedded in metacircular evaluator"
 awk -f process.awk \
 	-v TEMPLISPFMT="preprocessed/ff/program_premeta%d.lisp" \
        	-v TEMPLISPMETAFMT="preprocessed/ff/program_premetameta%d.lisp" \
-	< ff_meta.txt
+	< metacircular/ff_meta.txt
 
 echo "----------------------------------------------------------------------"
 echo "Running ff preembedded in metacircular evaluator once again embedded in metacircular evaluator"
@@ -28,14 +28,14 @@ awk -f process.awk \
 	-v RUNMETACIRCULAR=1 \
 	-v TEMPLISPFMT="preprocessed/ff/program_premeta%d.lisp" \
        	-v TEMPLISPMETAFMT="preprocessed/ff/program_premetameta%d.lisp" \
-	< ff_meta.txt
+	< metacircular/ff_meta.txt
 
 echo "======================================================================"
 echo "Running standalone_tests"
 awk -f process.awk \
 	-v TEMPLISPFMT="preprocessed/standalone/program%d.lisp" \
        	-v TEMPLISPMETAFMT="preprocessed/standalone/program_meta%d.lisp" \
-	< standalone_tests.txt
+	< standalone/standalone_tests.txt
 
 echo "----------------------------------------------------------------------"
 echo "Running standalone_tests embedded in metacircular evaluator"
@@ -43,14 +43,14 @@ awk -f process.awk \
 	-v RUNMETACIRCULAR=1 \
 	-v TEMPLISPFMT="preprocessed/standalone/program%d.lisp" \
        	-v TEMPLISPMETAFMT="preprocessed/standalone/program_meta%d.lisp" \
-	< standalone_tests.txt
+	< standalone/standalone_tests.txt
 
 echo "======================================================================"
 echo "Running tls_tests"
 awk -f process.awk \
 	-v TEMPLISPFMT="preprocessed/tls/program%d.lisp" \
        	-v TEMPLISPMETAFMT="preprocessed/tls/program_meta%d.lisp" \
-       	< tls_tests.txt
+       	< tls/tls_tests.txt
 
 echo "----------------------------------------------------------------------"
 echo "Running tls_tests embedded in metacircular evaluator"
@@ -58,13 +58,13 @@ awk -f process.awk \
 	-v RUNMETACIRCULAR=1 \
 	-v TEMPLISPFMT="preprocessed/tls/program%d.lisp" \
        	-v TEMPLISPMETAFMT="preprocessed/tls/program_meta%d.lisp" \
-	< tls_tests.txt
+	< tls/tls_tests.txt
 
 echo "======================================================================"
 echo "Running meta_tests (cannot run embedded in metacircular evaluator here)"
 awk -f process.awk \
 	-v TEMPLISPFMT="preprocessed/meta/program%d.lisp" \
        	-v TEMPLISPMETAFMT="preprocessed/meta/program_meta%d.lisp" \
-	< meta_tests.txt
+	< metacircular/meta_tests.txt
 
 echo "======================================================================"
