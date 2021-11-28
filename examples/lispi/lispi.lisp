def6a19c0ba3c5c3da21beda93b55c8c62478c7e
Keith Evans <11130585+derhexenmeister@users.noreply.github.com>
2021-11-28 12:08:31 -0800

    Added pair, assoc, and helper functions

--- examples/lispi/lispi.lisp
+++ examples/lispi/lispi.lisp
@@ -4,7 +4,7 @@
 ;; March 1, 1960
 ;; by Phyllis Fox
 ;;
-((LAMBDA (FF SUBST NOT OR AND EQUAL NULL APPEND AMONG COLLAPSE)
+((LAMBDA (FF SUBST NOT OR AND EQUAL NULL APPEND AMONG LIST PAIR CAAR CADAR ASSOC COLLAPSE)
 INSERT_TEST_DATA_HERE
 )
  ;
@@ -57,8 +57,7 @@ INSERT_TEST_DATA_HERE
  ; (equal x y) this is a predicate that has the value T is x and y are the
  ; same s-expression and has the value NIL otherwise.
  ;
- ; NOTE: original didn't use COND, but our AND/OR don't short circuit during
- ; eval
+ ; NOTE: original didn't use COND
  ;
  (QUOTE (LAMBDA (X Y)
                 (COND ((AND (ATOM X) (ATOM Y)) (EQ X Y))
@@ -86,13 +85,44 @@ INSERT_TEST_DATA_HERE
  ; Page 16
  ; (among x y) - this predicate is true if  the S-expression X
  ; occurs among the elements of list y
- ; Note: rewrote with cond since AND doesn't short circuit eval
+ ; Note: rewrote with cond
  ;
  (QUOTE (LAMBDA (X Y)
                 (COND ((NULL Y) NIL)
                       ((QUOTE T) (OR (EQUAL X (CAR Y))
                                      (AMONG X (CDR Y))))
                       )))
+ ;
+ ; Helper function - list
+ ; X must be an atom, y can be an atom or a list
+ (QUOTE (LAMBDA (X Y)
+        (COND ((ATOM Y) (CONS X Y))
+              ((QUOTE T) (CONS X (CONS Y NIL))))
+        ))
+ ;
+ ; Page 16
+ ; (pair x y) this function gives the list of pairs of corresponding
+ ; elements of the lists x and y
+ ;
+ (QUOTE (LAMBDA (X Y)
+                (COND ((OR (NULL X) (NULL Y)) NIL)
+                      ((QUOTE T) (CONS (LIST (CAR X) (CAR Y))
+                                       (PAIR (CDR X) (CDR Y))))
+                      )))
+ ;
+ ; Helper functions
+ ;
+ (QUOTE (LAMBDA (X) (CAR (CAR X))))       ; caar
+ (QUOTE (LAMBDA (X) (CAR (CDR (CAR X))))) ; cadar
+ ;
+ ; Page 16
+ ; (assoc x y) If y is a list of the form ((u1 v1) .. (un vn)) and x
+ ; is one of the u's then (assoc x y) is the corresponing v
+ ;
+ (QUOTE (LAMBDA (X Y)
+                (COND ((EQUAL X (CAAR Y)) (CADAR Y))
+                      ((QUOTE T) (ASSOC X (CDR Y))))
+                ))
  ;
  ; Page xx
  ; Based on "Function to Collapse a List of Elements"
